a4507756c9ed12eeee0dc679ab9688fee39ac6e6
Pavel Martinez <pavomartinez@hotmail.com>
2025-05-01 14:24:47 -0700

    Updated Allowed Origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-  "*",
+  "https://dazzling-snickerdoodle-777101.netlify.app",
+  "https://fuchsia-striped-cardinal.glitch.me",
   "https://www.yoursite.com",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
